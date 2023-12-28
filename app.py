a9c6316ccb5368b05613dc7a3a3b1cced16e57b8
miggymofongo2 <contact@miguelalmodo.com>
2023-12-27 22:33:55 -0800

    updated app.py, index.html, as well as uploaded bookshelf of sample epubs

--- app.py
+++ app.py
@@ -1,75 +1,47 @@
-from flask import Flask, render_template, request, send_from_directory
+from flask import Flask, request, send_from_directory
 import os
+
+# Initialize Flask app
 app = Flask(__name__)
 
-EPUBS_DIR = {
-    'default': './bookshelf/'
-}
-
+# Directory containing EPUB files
+EPUBS_DIR = './bookshelf/'
 
 @app.route('/')
 def index():
-    return send_from_directory(app.root_path,'index.html')
-
+    # Serve the main HTML page
+    return send_from_directory(app.root_path, 'index.html')
 
 @app.route('/search', methods=['GET'])
 def search():
+    # Get the search query parameter and convert it to lowercase for case-insensitive matching
     query = request.args.get('searchQuery', '').lower()
+
+    # List to store filenames of matching EPUB files
     matching_files = []
-    for _, dir_path in EPUBS_DIR.items():
-        all_files = [f for f in os.listdir(dir_path) if f.lower().endswith('.epub')]
-        matching_files += [f for f in all_files if query in f.lower()]
+
+    # Fetch all EPUB files from the directory and filter them based on the search query
+    all_files = [f for f in os.listdir(EPUBS_DIR) if f.lower().endswith('.epub')]
+    matching_files += [f for f in all_files if query in f.lower()]
     
-    # Render the results as HTML
+    # Generate HTML for the list of matching files
     results_html = "".join([f'<li><a href="#" onclick="loadEpubInBibi(\'/bookshelf/{file}\'); return false;">{file}</a></li>' for file in matching_files])
     return results_html
 
-
+@app.route('/bookshelf/<filename>')
+def serve_epub(filename):
+    # Serve an EPUB file from the bookshelf directory
+    return send_from_directory(EPUBS_DIR, filename)
 
 @app.route('/reader/<path:subpath>')
 def serve_bibi_assets(subpath):
-    # This will serve all of Bibi's assets, such as JavaScript, CSS, etc.
+    # Serve assets for the Bibi EPUB reader (JavaScript, CSS, etc.)
     return send_from_directory('./reader', subpath)
-
-
-
-
-@app.route('/bookshelf/<filename>')
-def serve_epub(filename):
-    return send_from_directory('./bookshelf', filename)
-
-
-
-
-#@app.route('/reader')
-#def serve_bibi_reader():
-#    book_path = request.args.get('book')
-    # You might want to do some checks on `book_path` for security reasons.
-#    return send_from_directory('./reader', 'index.html')
-
-@app.route('/reader')
-def serve_bibi_reader():
-    book_path = request.args.get('book', '')
-    if book_path:
-        # Check if the EPUB file exists in the bookshelf
-        full_path = os.path.join('bookshelf', book_path.strip('/'))
-        if os.path.exists(full_path):
-            # Serve the Bibi reader's index.html
-            # The book path will be handled by Bibi's JavaScript
-            return send_from_directory('reader', 'index.html')
-        else:
-            return "EPUB file not found", 404
-    return "No book specified", 400
-
-
-
-
-
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, './favicon.ico'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    # Serve the favicon
+    return send_from_directory(os.path.join(app.root_path, 'favicon.ico'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(debug=True)
