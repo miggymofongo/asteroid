0313e2f0fd4f8c59faa49875f66209e7767622d7
Miguel Almodovar <miguel@miguelalmodo.com>
2024-01-22 18:26:25 -0400

    declared facts as a constant

--- scripts.js
+++ scripts.js
@@ -17,16 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(quotes => {
             let currentQuoteIndex = 0;
             const quoteBanner = document.getElementById('quoteBanner');
+            const quoteText = quoteBanner.querySelector('blockquote p');
     
             function updateQuote() {
-                quoteBanner.textContent = quotes[currentQuoteIndex++];
-                if (currentQuoteIndex >= quotes.length) currentQuoteIndex = 0;
+                quoteText.textContent = quotes[currentQuoteIndex];
+                currentQuoteIndex = (currentQuoteIndex + 1) % quotes.length;
             }
     
             updateQuote();
             setInterval(updateQuote, 5000);
-        });
+        })
+        .catch(error => console.error('Error loading JSON:', error));
 });
+
+
     
 
 
