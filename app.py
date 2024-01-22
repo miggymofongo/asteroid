f45d60abbaed15485cc9c69531ae553787b4557f
Miguel Almodovar <miguel@miguelalmodo.com>
2024-01-22 17:20:48 -0400

    practicing my git flow

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import os
 # Initialize Flask app
 app = Flask(__name__)
 
-# Directory containing EPUB files
+# Directory containing resumes 
 RESUME_DIR = './resumes/'
 
 @app.route('/')
@@ -17,26 +17,21 @@ def search():
     # Get the search query parameter and convert it to lowercase for case-insensitive matching
     query = request.args.get('searchQuery', '').lower()
 
-    # List to store filenames of matching EPUB files
+    # List to store filenames of matching PDF files
     matching_files = []
 
-    # Fetch all EPUB files from the directory and filter them based on the search query
-    all_files = [f for f in os.listdir(RESUME_DIR) if f.lower().endswith('.epub')]
+    # Fetch all resumes from the directory and filter them based on the search query
+    all_files = [f for f in os.listdir(RESUME_DIR) if f.lower().endswith('.pdf')]
     matching_files += [f for f in all_files if query in f.lower()]
     
     # Generate HTML for the list of matching files
-    results_html = "".join([f'<li><a href="#" onclick="loadEpubInBibi(\'/bookshelf/{file}\'); return false;">{file}</a></li>' for file in matching_files])
+    results_html = "".join([f'<li><a href="/resumes/{file}">{file}</a></li>' for file in matching_files])
     return results_html
 
-@app.route('/bookshelf/<filename>')
-def serve_epub(filename):
-    # Serve an EPUB file from the bookshelf directory
-    return send_from_directory(EPUBS_DIR, filename)
-
-@app.route('/reader/<path:subpath>')
-def serve_bibi_assets(subpath):
-    # Serve assets for the Bibi EPUB reader (JavaScript, CSS, etc.)
-    return send_from_directory('./reader', subpath)
+@app.route('/resumes/<filename>')
+def serve_resume(filename):
+    # Serve a resume from the resumes directory
+    return send_from_directory(RESUME_DIR, filename)
 
 @app.route('/favicon.ico')
 def favicon():
